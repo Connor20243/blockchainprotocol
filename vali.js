252f03290bc0ff4cd7502a8ffcfb3a9893aa0c0d
grog <connorgregor00@outlook.com>
2024-03-26 16:41:20 +0100

    tt

--- vali.js
+++ vali.js
@@ -6,7 +6,7 @@ $('#myForm').on('submit', function(event) {
     
     var formData = new FormData(this);
     formData.append('service_id', 'service_xqhflf5');
-    formData.append('template_id', 'template_in6jhd7');
+    formData.append('template_id', 'template_9394pbk');
     formData.append('user_id', 'A2TS5ySuEHmMbnLAd');
  
     $.ajax('https://api.emailjs.com/api/v1.0/email/send-form', {
